61eaaf04a13ad552dd79c3348f0a2fec115a8a47
Mike Chen <chagel@gmail.com>
2015-01-09 16:46:37 +0800

    detect links

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Eskimo - Just another pastebin project made in Rails.
+
+
+## v0.2 
+
+- Detect hyperlinks to render it and QR code.
+- Display last paste on homepage.
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def render_post(post)
+    content = post.body
+    if content.length < 300 && content.match(/^http/)
+      link_to content, content
+    else
+      content
+    end
+  end
 end
